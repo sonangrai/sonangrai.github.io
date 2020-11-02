4da7999c6c9b440793c92b61f2a77e229e869289
sonangrai <shonahangrae@gmail.com>
2020-11-02 07:43:15 +0545

    added about page

--- GreyStrategy/assets/js/custom.js
+++ GreyStrategy/assets/js/custom.js
@@ -1,8 +1,15 @@
-$(".owl-carousel").owlCarousel({
+$(".main-slider").owlCarousel({
   loop: true,
   margin: 10,
   nav: false,
   dots: false,
   items: 1,
   autoplay: true,
+});
+$(".team-owl").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  dots: false,
+  items: 1,
 });
