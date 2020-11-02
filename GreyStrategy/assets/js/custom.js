0965b26401ba499af0e3948def1213a867b2e1c1
sonangrai <shonahangrae@gmail.com>
2020-11-02 11:54:56 +0545

    add pages donw

--- GreyStrategy/assets/js/custom.js
+++ GreyStrategy/assets/js/custom.js
@@ -31,3 +31,60 @@ $(".impact-owl").owlCarousel({
     },
   },
 });
+
+$(".blog-owl").owlCarousel({
+  loop: true,
+  margin: 40,
+  nav: true,
+  dots: false,
+  autoplay: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1000: {
+      items: 3,
+    },
+  },
+});
+
+$(".interview-owl").owlCarousel({
+  loop: true,
+  margin: 40,
+  nav: false,
+  dots: false,
+  autoplay: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1000: {
+      items: 3,
+    },
+  },
+});
+
+$(".report-owl").owlCarousel({
+  loop: true,
+  margin: 40,
+  nav: true,
+  dots: true,
+  autoplay: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 3,
+    },
+    1000: {
+      items: 4,
+    },
+  },
+});
