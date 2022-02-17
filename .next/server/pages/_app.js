d5e39cb77642eb5c789deac9aea6267e7141df94
sonangrai <shonahangrae@gmail.com>
2022-02-17 12:07:07 +0545

    next js

--- .next/server/pages/_app.js
+++ .next/server/pages/_app.js
@@ -5,7 +5,7 @@ exports.id = 888;
 exports.ids = [888];
 exports.modules = {
 
-/***/ 510:
+/***/ 656:
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
 __webpack_require__.r(__webpack_exports__);
@@ -40,7 +40,7 @@ module.exports = require("react/jsx-runtime");
 var __webpack_require__ = require("../webpack-runtime.js");
 __webpack_require__.C(exports);
 var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
-var __webpack_exports__ = (__webpack_exec__(510));
+var __webpack_exports__ = (__webpack_exec__(656));
 module.exports = __webpack_exports__;
 
 })();
--- .next/server/pages/api/hello.js
+++ .next/server/pages/api/hello.js
@@ -5,14 +5,13 @@ exports.id = 453;
 exports.ids = [453];
 exports.modules = {
 
-/***/ 677:
+/***/ 505:
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
 __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "default": () => (/* binding */ handler)
 /* harmony export */ });
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 function handler(req, res) {
     res.status(200).json({
         name: 'John Doe'
@@ -29,7 +28,7 @@ function handler(req, res) {
 var __webpack_require__ = require("../../webpack-api-runtime.js");
 __webpack_require__.C(exports);
 var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
-var __webpack_exports__ = (__webpack_exec__(677));
+var __webpack_exports__ = (__webpack_exec__(505));
 module.exports = __webpack_exports__;
 
 })();
